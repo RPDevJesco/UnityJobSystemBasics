6fc3e0c29172d761df2a5ba61eef73959334c54f
DESKTOP-7EB9MKR\jglov <jglover88@hotmail.com>
2019-02-24 19:36:29 +0800

    Code files for video.

---
+++ UnityJobs/Assets/ApplyRotation.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public class ApplyRotation : MonoBehaviour
+{
+    private JobHandle jobHandle;
+    private TransformAccessArray transformArray;
+    private NativeArray<Vector3> rotationAxis;
+    RotationJob job = new RotationJob();
+
+    void Start()
+    {
+        rotationAxis = new NativeArray<Vector3>(1, Allocator.Persistent);
+        this.rotationAxis[0] = Vector3.up;
+        Transform[] transforms = { transform };
+        transformArray = new TransformAccessArray(transforms);
+    }
+
+    void OnDisable()
+    {
+        // Dispose long-term data for jobs
+        // This includes access to transforms
+        this.transformArray.Dispose();
+        this.rotationAxis.Dispose();
+    }
+
+    void Update()
+    {
+        this.job.RotationalAxis = this.rotationAxis;
+        this.jobHandle = job.Schedule(transformArray, this.jobHandle);
+    }
+
+    void LateUpdate()
+    {
+        this.jobHandle.Complete();
+    }
+}
---
+++ UnityJobs/Assets/RotationJob.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public struct RotationJob : IJobParallelForTransform
+{
+    public NativeArray<Vector3> RotationalAxis;
+
+    public void Execute(int index, TransformAccess transform)
+    {
+        transform.localRotation *= Quaternion.Euler(this.RotationalAxis[index]);
+    }
+}
